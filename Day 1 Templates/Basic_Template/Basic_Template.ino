c666e93421cd6cc2aa48676a863138f07d71b58b
EthanMuchnik <emscientist@gmail.com>
2019-10-30 21:49:25 -0400

    Added the basic template

---
+++ Day 1 Templates/Basic_Template/Basic_Template.ino
@@ -0,0 +1,16 @@
+//Everything written on the same line after two forward slashes is not going to run and go to the arduino. 
+//These are just comments where you can read instructions or take notes to explain what your code does.
+
+//                    WHAT IS THIS TEMPLATE
+//This is a blank template that will be standard for all Arduino Code
+
+                      WHAT IS INSIDE OF THIS TEMPLATE
+//There are two functions. A function consists of its name - for example: setup: - and what goes inside of the curly brackets.
+
+void setup() {
+  // The arduino only does what is inside of setup once. That means you use this to simply set up the code.
+}
+
+void loop() {
+  // The arduino keeps repeating the inside of this function. If you want a light to constantely check if it should be on, here is the place to do it.
+}
