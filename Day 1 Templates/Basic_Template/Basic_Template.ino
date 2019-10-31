2cbb183447d848a0e1b266b9164c08e6b5d2800b
EthanMuchnik <emscientist@gmail.com>
2019-10-30 21:50:22 -0400

    Minor changes to the template

--- Day 1 Templates/Basic_Template/Basic_Template.ino
+++ Day 1 Templates/Basic_Template/Basic_Template.ino
@@ -1,10 +1,10 @@
 //Everything written on the same line after two forward slashes is not going to run and go to the arduino. 
 //These are just comments where you can read instructions or take notes to explain what your code does.
 
-//                    WHAT IS THIS TEMPLATE
+//                    WHAT IS THIS TEMPLATE?
 //This is a blank template that will be standard for all Arduino Code
 
-                      WHAT IS INSIDE OF THIS TEMPLATE
+                      WHAT IS INSIDE OF THIS TEMPLATE?
 //There are two functions. A function consists of its name - for example: setup: - and what goes inside of the curly brackets.
 
 void setup() {
