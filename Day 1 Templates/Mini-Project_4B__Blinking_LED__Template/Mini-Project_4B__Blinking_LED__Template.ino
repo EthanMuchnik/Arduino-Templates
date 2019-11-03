c89d2ffa2bf78255c6f96f81d498beac1d67c71f
EthanMuchnik <emscientist@gmail.com>
2019-11-03 00:12:59 -0400

    Made Mini-Project 4B project

---
+++ Day 1 Templates/Mini-Project_4B__Blinking_LED__Template/Mini-Project_4B__Blinking_LED__Template.ino
@@ -0,0 +1,28 @@
+//                          Mini Project: Blink Code
+// Your code should make an LED turn on and off. It should switch states every second and run forever.
+
+//                            Things to Rememeber
+// HIGH makes electricity go. LOW makes no electricity go.
+// OUTPUT: Make electricity flow. INPUT reads the amount of electricity flowing       
+// Delay: Countinues the current action for a certain emount of miliseconds
+// pinMode(): makes a pin  an INPUT or OUTPUT. Always goes in setup 
+// digitialWrite(): Makes a pin HIGH or LOW. Usually goes in loop
+
+//Setup Runs Once
+//Loop never stops running
+//declare variables that are used in both functions outside of them
+
+
+
+int pin = ;// Write the pin that your wire is plugged into. (2-9)
+
+void setup() {
+  pinMode(pin, );// Write Input or Output. 
+}
+
+void loop() {
+  digitalWrite(pin, );   // turn the LED on. Write HIGH or LOW. 
+  delay();                       // Keep the light turned on for 1 seconds. Write a number
+  digitalWrite(pin, );    // turn the LED on. Write HIGH or LOW. 
+  delay();                   // Keep the light turned off for 1 seconds. Write a number               
+}
