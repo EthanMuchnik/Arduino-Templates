1f3d3f3f017bd8ca75a21f266a2daca048097938
EthanMuchnik <emscientist@gmail.com>
2019-11-03 00:23:45 -0400

    Made template for Project 4A

--- Day 1 Templates/Mini-Project_4A__Forever_LED__Template/Mini-Project_4A__Forever_LED__Template.ino
+++ Day 1 Templates/Mini-Project_4A__Forever_LED__Template/Mini-Project_4A__Forever_LED__Template.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+//                       Mini Project: Forever LED
+// Your code should make an LED turn on forever.
 
+//                            Things to Rememeber
+// HIGH makes electricity go. LOW makes no electricity go.
+// OUTPUT: Make electricity flow. INPUT reads the amount of electricity flowing       
+// Delay: Countinues the current action for a certain emount of miliseconds
+// pinMode(): makes a pin  an INPUT or OUTPUT. Always goes in setup 
+// digitialWrite(): Makes a pin HIGH or LOW. Usually goes in loop
+
+//Setup Runs Once
+//Loop never stops running
+//declare variables that are used in both functions outside of them
+
+
+
+int pin = ;// Write the pin that your wire is plugged into. (2-9)
+
+void setup() {
+  pinMode(pin, );// Write Input or Output. 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(pin, );   // turn the LED on. Write HIGH or LOW.
 }
