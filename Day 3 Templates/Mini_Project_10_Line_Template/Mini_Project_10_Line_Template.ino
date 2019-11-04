f0ee05ae7135973208b73080f07278b5cfaf99ef
EthanMuchnik <emscientist@gmail.com>
2019-11-04 01:11:41 -0500

    Made line template

---
+++ Day 3 Templates/Mini_Project_10_Line_Template/Mini_Project_10_Line_Template.ino
@@ -0,0 +1,33 @@
+//A bunch of files with useful functions are imported
+#include "SPI.h"
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+//The size of a display.
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+//Makes a display object
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Make the actual display and run the Logo
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  //Clear the display, 
+  //Create the line
+  //Display the line  
+}
+
+//Loop is not used so everything you see so only runs once
+void loop() {
+}
