0957f4d68d691d4655e35d30f3cff64ceca49b95
EthanMuchnik <emscientist@gmail.com>
2019-11-04 01:55:48 -0500

    Mini Project Commited

---
+++ Day 3 Templates/Mini_Project_12_Text_Template/Mini_Project_12_Text_Template.ino
@@ -0,0 +1,44 @@
+//             Mini Project: Text
+//  Look at how text was made. Now make White text at point x = 10 and y = 40, with a size of 1. The text should say "Hello OLED display 
+
+//                            Things to Rememeber
+// display.println(Words) - puts any text into the display in between the parenthesis. 
+// display.setTextSize(Number)- It makes the the text bigger or smaller.
+// display.setTextColor(color) - It makes the text a certain color based on what is inside of the parenthesis. 
+// display.setCursor(x,y) - Specified the left top point where the text will start. 
+// display.display() - makes the changes to the OLED display visible
+// display.clearDisplay() - delets everything on the display
+
+#include "SPI.h"
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+//The size of a display.
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+
+#define OLED_RESET     4 
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("Word");
+  display.display();
+
+// Write your new text here
+
+}
+
+void loop() {
+}
