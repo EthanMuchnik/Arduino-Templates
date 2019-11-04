53f473d05ad4d298bfac108b4c281dba993e761e
EthanMuchnik <emscientist@gmail.com>
2019-11-03 21:39:57 -0500

    OLED template functions

--- Day 3 Templates/LibraryOLEDTemplateSimplfied/LibraryOLEDTemplateSimplfied.ino
+++ Day 3 Templates/LibraryOLEDTemplateSimplfied/LibraryOLEDTemplateSimplfied.ino
@@ -1,10 +1,13 @@
+//A bunch of files with useful functions are imported
 #include "SPI.h"
 #include "Wire.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+//The size of a display.
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -13,13 +16,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup() {
   Serial.begin(9600);
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  // Make the actual display and run the Logo
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  //Next slide
   display.display();
 }
 
+//Loop is not used so everything you see so only runs once
 void loop() {
 }
