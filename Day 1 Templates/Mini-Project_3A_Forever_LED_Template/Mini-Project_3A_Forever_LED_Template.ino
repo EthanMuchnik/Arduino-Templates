177a31207654b254871e3a209de26453254c631e
EthanMuchnik <emscientist@gmail.com>
2019-11-19 23:39:24 -0500

    Day 1 Template fixes

--- Day 1 Templates/Mini-Project_3A_Forever_LED_Template/Mini-Project_3A_Forever_LED_Template.ino
+++ Day 1 Templates/Mini-Project_3A_Forever_LED_Template/Mini-Project_3A_Forever_LED_Template.ino
@@ -2,11 +2,11 @@
 // Your code should make an LED turn on forever.
 
 //                            Things to Rememeber
-// HIGH makes electricity go. LOW makes no electricity go.
-// OUTPUT: Make electricity flow. INPUT reads the amount of electricity flowing       
-// Delay: Countinues the current action for a certain emount of miliseconds
-// pinMode(): makes a pin  an INPUT or OUTPUT. Always goes in setup 
-// digitialWrite(): Makes a pin HIGH or LOW. Usually goes in loop
+// HIGH: makes electricity go.            LOW: makes no electricity go.
+// OUTPUT: Make electricity flow.         INPUT: reads the amount of electricity flowing       
+// delay(miliseconds): Countinues the current action for a certain amount of miliseconds      ||     1000 miliseconds = 1 seconds
+// pinMode(pin, OUPUT/LOW): makes a pin  an INPUT or OUTPUT. 
+// digitalWrite(pin, HIGH/LOW): Makes a pin Output HIGH or LOW. 
 
 //Setup Runs Once
 //Loop never stops running
@@ -14,12 +14,12 @@
 
 
 
-int pin = ;// Write the pin that your wire is plugged into. (2-13)
+int pin = ;  // Write the pin that your wire is plugged into (2-13).
 
 void setup() {
-  pinMode(pin,);// Write INPUT or OUTPUT. 
+  pinMode(pin,);  // Write INPUT or OUTPUT. 
 }
 
 void loop() {
-  digitalWrite(pin,);   // turn the LED on. Write HIGH or LOW.
+  digitalWrite(pin,);  // Turn the LED on. Write HIGH or LOW.
 }
--- Day 1 Templates/Mini-Project_3B_Blinking_LED_Template/Mini-Project_3B_Blinking_LED_Template.ino
+++ Day 1 Templates/Mini-Project_3B_Blinking_LED_Template/Mini-Project_3B_Blinking_LED_Template.ino
@@ -1,28 +1,28 @@
 //                          Mini Project: Blinking LED
-// Your code should make an LED turn on and off. It should switch states every second and run forever.
+// Your code should make an LED turn on and off. It should switch states every second.
 
-//                            Things to Rememeber
-// HIGH makes electricity go. LOW makes no electricity go.
-// OUTPUT: Make electricity flow. INPUT reads the amount of electricity flowing       
-// Delay: Countinues the current action for a certain emount of miliseconds
-// pinMode(): makes a pin  an INPUT or OUTPUT. Always goes in setup 
-// digitialWrite(): Makes a pin HIGH or LOW. Usually goes in loop
+       Things to Rememeber:
+// HIGH: makes electricity go.            LOW: makes no electricity go.
+// OUTPUT: Make electricity flow.         INPUT: reads the amount of electricity flowing       
+// delay(miliseconds): Countinues the current action for a certain amount of miliseconds      ||     1000 miliseconds = 1 seconds
+// pinMode(pin, OUPUT/LOW): makes a pin  an INPUT or OUTPUT. 
+// digitalWrite(pin, HIGH/LOW): Makes a pin Output HIGH or LOW. 
 
-//Setup Runs Once
-//Loop never stops running
-//declare variables that are used in both functions outside of them
+//setup(): Runs once
+//loop(): Never stops running
+//declare variables that are used in both functions outside of themthem
 
 
 
-int pin =;// Write the pin that your wire is plugged into. (2-13)
+int pin = ;   // Write the pin that your wire is plugged into (2-13).
 
 void setup() {
-  pinMode(pin, );// Write INPUT or OUTPUT. 
+  pinMode(pin, );   // Write INPUT or OUTPUT. 
 }
 
 void loop() {
-  digitalWrite(pin, );   // turn the LED on. Write HIGH or LOW. 
-  delay();                       // Keep the light turned on for 1 seconds. Write a number
-  digitalWrite(pin,);    // turn the LED on. Write HIGH or LOW. 
-  delay();                       // Keep the light turned off for 1 seconds. Write a number               
+  digitalWrite(pin, );   // Turn the LED on. Write HIGH or LOW. 
+  delay();                       // Keep the light turned on for 1 second. Write a number.
+  digitalWrite(pin,);    // Turn the LED on. Write HIGH or LOW. 
+  delay();                       // Keep the light turned off for 1 second. Write a number.               
 }
