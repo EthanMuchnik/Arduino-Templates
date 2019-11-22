8f3f68d14b0d9a3ab77167636e6f6080a7cb49e0
ryan10145 <chang.ryan1@yahoo.com>
2019-11-22 16:13:26 -0500

    Changed if-statement template to include else if and else statements

--- Day 1 Templates/Basic_Template/Basic_Template.ino
+++ Day 1 Templates/Basic_Template/Basic_Template.ino
@@ -1,16 +1,24 @@
-//Everything written on the same line after two forward slashes is not going to run and go to the arduino. 
-//These are just comments where you can read instructions or take notes to explain what your code does.
+#define PIN_BUZZER 7
+#define PIN_ADD 3
 
-//                    WHAT IS THIS TEMPLATE?
-//This is a blank template that will be standard for all Arduino Code
+bool running = true;
 
-//                    WHAT IS INSIDE OF THIS TEMPLATE?
-//There are two functions. A function consists of its name - for example: setup: - and what goes inside of the curly brackets.
+int frequency = 10;
+
+void add() {
+  if(frequency < 100) frequency += 10;
+}
 
 void setup() {
-  // The arduino only does what is inside of setup once. That means you use this to simply set up the code.
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_ADD, INPUT);
+
+  Serial.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_ADD), add, RISING);
 }
 
 void loop() {
-  // The arduino keeps repeating the inside of this function. If you want a light to constantely check if it should be on, here is the place to do it.
+  Serial.println(frequency);
+  tone(PIN_BUZZER, frequency);
 }
--- Day 2 Templates/If_Statements_Template/If_Statements_Template.ino
+++ Day 2 Templates/If_Statements_Template/If_Statements_Template.ino
@@ -4,29 +4,35 @@
 //                            Things to Rememeber
 // if(Check something)
 //{
-//  Do something
+//  Do something;
 //}
+// Serial.begin() Sets up the printing area
+// Serial.println(): prints anything you put into it
 
 
 //Setup Runs Once
 //Loop never stops running
 //declare variables that are used in both functions outside of them
 
-int aNumber = 1; //You need one equal sign because you are assigning
+int age = 0; //You need one equal sign because you are assigning
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  if(aNumber == 1) //Tests if aNumber is equal to one. There are two equal signs because you are checking, not assigning
+  if(age == 1) //Tests if aNumber is equal to one. There are two equal signs because you are checking, not assigning
   {
-    Serial.println(aNumber);//If the statement is correct do what is inside of the curly brackets
+    Serial.println("I am 1 year old");//If the statement is correct, do what is inside of the curly brackets
   }
 
-  else if(aNumber != 1)//If the if statement doesn't run, it runs this. IF this is correct. It would do what's inside of the curly brackets
-  {
-    Serial.println(0);
+//  else if(aNumber != 1)//If the if statement doesn't run, it runs this. IF this is correct. It would do what's inside of the curly brackets
+//  {
+//    Serial.println(0);
+//  }
+
+  else {
+    Serial.println("I am blank year old");
   }
 }
 
