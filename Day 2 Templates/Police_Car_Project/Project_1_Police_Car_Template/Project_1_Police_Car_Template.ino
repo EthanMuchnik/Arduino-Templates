58c837dda127d36160fa36ccb3dc77c1a62ab4ed
Adam Zamlynny <azamlynny@hotmail.com>
2019-12-06 17:59:40 -0500

    Fixed spelling/spacing issues

--- Day 2 Templates/Police_Car_Project/Project_1_Police_Car_Template/Project_1_Police_Car_Template.ino
+++ Day 2 Templates/Police_Car_Project/Project_1_Police_Car_Template/Project_1_Police_Car_Template.ino
@@ -2,15 +2,15 @@
 // Make a mini police siren. 
 //        - Two LEDs(Blue and Red) should go from being on and off 
 //        - While a buzzer goes from a high frequency to a low frequncy
-//        - THis should happen every 0.2 seconds(200 miliseconds)
+//        - This should happen every 0.2 seconds (200 milliseconds)
 //        - This should go on forever
-//
+
 
 
 //                            Things to Rememeber
 // HIGH: makes electricity go.            LOW: makes no electricity go.
 // OUTPUT: Make electricity flow.         INPUT: reads the amount of electricity flowing       
-// delay(miliseconds): Countinues the current action for a certain amount of miliseconds      ||     1000 miliseconds = 1 second
+// delay(milliseconds): Countinues the current action for a certain amount of miliseconds      ||     1000 miliseconds = 1 second
 // pinMode(pin, OUPUT/INPUT): makes a pin  an INPUT or OUTPUT. 
 // digitalWrite(pin, HIGH/LOW): Makes a pin Output HIGH or LOW. 
 // tone(pin, frequency): Takes in the pin and then the frequency to play a certain note
