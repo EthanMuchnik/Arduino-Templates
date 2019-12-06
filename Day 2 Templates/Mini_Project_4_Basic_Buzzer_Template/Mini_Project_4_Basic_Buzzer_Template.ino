407ff5005db07b921df824f7c812777a7a53a60f
Adam Zamlynny <azamlynny@hotmail.com>
2019-12-06 17:55:24 -0500

    Fixed spelling/spacing issues

--- Day 2 Templates/Mini_Project_4_Basic_Buzzer_Template/Mini_Project_4_Basic_Buzzer_Template.ino
+++ Day 2 Templates/Mini_Project_4_Basic_Buzzer_Template/Mini_Project_4_Basic_Buzzer_Template.ino
@@ -1,7 +1,7 @@
 //                        Mini Project: Basic Buzzer
 // 1. This code should make the buzzer play a note. Try to change the frequency and see what happens.
 
-// 2. After that, try to use the noTone()command to end your song after 5 seconds. Hint: You have to use setup().
+// 2. After that, try to use the noTone() command to end your song after 5 seconds. Hint: You have to use setup().
 
 
 
@@ -12,9 +12,9 @@
 // delay(miliseconds): Countinues the current action for a certain amount of miliseconds      ||     1000 miliseconds = 1 second
 // tone(pin, frequency): Takes in the pin and then the frequency to play a certain note
 // noTone(pin): This simply ends your song/buzz
-// pinMode(pin, OUPUT/INPUT): makes a pin  an INPUT or OUTPUT. 
+// pinMode(pin, OUPUT/INPUT): makes a pin an INPUT or OUTPUT. 
 
-// Int format:            int variableName = number;
+// Integer format:        int variableName = number;
 
 // Setup Runs Once
 // Loop never stops running
@@ -29,11 +29,11 @@ int frequency = 3000;
 
 void setup() 
 {
-  pinMode(pin,OUTPUT); //Makes the pin an OUTPUT
+  pinMode(pin,OUTPUT); // Makes the pin an OUTPUT
 }
 
 void loop() 
 {
-  //  Play a note at any frequency and see happens when you change it. P.S only go between 50 and 13000.
+  // Play a note at any frequency and see happens when you change it. P.S only go between 50 and 13000.
   tone(pin,frequency);
 }
